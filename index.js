6241d41f3d5d613db60fd36ca12086028653299a
Cypher890 <abdulsuhaib07@gmail.com>
2022-12-04 19:52:23 +0530

    added the changes

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const User = require("./models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 // const session = require('express-session')
-const SECRET_KEY = "JWTSECRET"
-let JWTID, INFO = null
+const SECRET_KEY = "JWTSECRET";
+let JWTID,
+  INFO = null;
 // const Name = 'ABDUL SUHAIB'
 
 mongoose.connect("mongodb://localhost:27017/myProject");
 
+// app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static('public'));
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.urlencoded({ extended: true }));
@@ -19,16 +22,15 @@ app.use(express.urlencoded({ extended: true }));
 // }))
 
 app.use((req, res, next) => {
-    res.set('Cache-Control', 'no-store')
-    next()
-})
+  res.set("Cache-Control", "no-store");
+  next();
+});
 
-
-app.get("/", async(req, res) => {
-  const filter = {}
-  const all = await User.find(filter)
+app.get("/", async (req, res) => {
+  const filter = {};
+  const all = await User.find(filter);
   // console.log(all)
-  res.render("home",{Name:INFO,Table:all});
+  res.render("home", { Name: INFO, Table: all });
 });
 app.get("/register", (req, res) => {
   res.render("register");
@@ -43,10 +45,12 @@ app.post("/register", async (req, res) => {
   });
   await user.save();
 
-  const token = jwt.sign({username:user.username , id : user._id}, SECRET_KEY)
-  console.log(token)
+  const token = jwt.sign({ username: user.username, id: user._id }, SECRET_KEY);
+  const decoded = jwt.verify(token, SECRET_KEY);
+  INFO = decoded.username;
+  JWTID = decoded.id;
   // req.session.user_id = user._id;
-  // res.redirect("/secret");
+  res.redirect("/secret");
 });
 
 app.get("/login", (req, res) => {
@@ -54,48 +58,48 @@ app.get("/login", (req, res) => {
 });
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username:username });
+  const user = await User.findOne({ username: username });
   if (user) {
     const validPassword = await bcrypt.compare(password, user.password);
     if (validPassword) {
-      const token = jwt.sign({username:user.username , id : user._id}, SECRET_KEY)
-      const decoded = jwt.verify(token, SECRET_KEY)
-      INFO = decoded.username
-      JWTID = decoded.id
-      // req.session.user_id = user._id;  
+      const token = jwt.sign(
+        { username: user.username, id: user._id },
+        SECRET_KEY
+      );
+      const decoded = jwt.verify(token, SECRET_KEY);
+      INFO = decoded.username;
+      JWTID = decoded.id;
+      // req.session.user_id = user._id;
       // res.redirect('/secret');
-      console.log("you are logged in "+ user + token)
-      console.log("you are logged in "+ decoded.id)
-      res.redirect('/secret')
+      console.log("you are logged in " + user + token);
+      console.log("you are logged in " + decoded.id);
+      res.redirect("/secret");
     } else {
-      res.redirect('/login');
+      res.redirect("/login");
     }
   } else {
-    res.redirect('/register');
+    res.redirect("/register");
   }
 });
-app.post('/logout',(req,res)=>{
-  JWTID = null
-  INFO = null
-    //  req.session.user_id = null
-    // req.session.destroy()
-    res.redirect('/')
-})
+app.post("/logout", (req, res) => {
+  JWTID = null;
+  INFO = null;
+  //  req.session.user_id = null
+  // req.session.destroy()
+  res.redirect("/");
+});
 
- app.get('/secret', (req, res)=>{
-    if(!JWTID){
-        res.redirect('/login')
-    }
-     res.render('secret')
- })
+app.get("/secret", (req, res) => {
+  if (!JWTID) {
+    res.redirect("/login");
+  }
+  res.render("secret");
+});
 
-
-const findAll =async ()=>{
-  
-  const filter = {}
-  const all = await User.find(filter)
-}
-
+const findAll = async () => {
+  const filter = {};
+  const all = await User.find(filter);
+};
 
 app.listen(3000, () => {
   console.log("serving your app on 3000");
